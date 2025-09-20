8edcaf1e15f0cde738a1c2828027740ae146db43
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-09-20 17:08:50 +0300

    Add components for vehicles

---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.Sim/Components/Vehicle/IdmParameters.cs
@@ -0,0 +1,49 @@
+﻿namespace TrafficSimulation.Sim.Components;
+
+/// <summary>
+/// Represents the configuration parameters for the Intelligent Driver Model (IDM).
+/// The IDM is a traffic simulation model used to describe the longitudinal driving behavior
+/// of vehicles, balancing desired speed, acceleration, and safe following distances.
+/// </summary>
+public readonly struct IdmParameters(float desiredSpeed, float maxAcceleration, float comfortableBraking, float headwayTime, float minGap, float accelerationExponent) {
+    /// <summary>
+    /// Specifies the target speed for the vehicle, expressed in meters per second (m/s).
+    /// This value determines the intended cruising speed under optimal driving conditions,
+    /// considering road constraints and other traffic influences.
+    /// </summary>
+    public readonly float DesiredSpeed = desiredSpeed;
+
+    /// <summary>
+    /// Specifies the maximum acceleration capability of the vehicle, measured in m/s^2.
+    /// This value represents the highest rate of increase in speed that the vehicle can achieve
+    /// under ideal conditions, such as optimal traction and no external constraints.
+    /// </summary>
+    public readonly float MaxAcceleration = maxAcceleration;
+
+    /// <summary>
+    /// Represents the deceleration rate, in m/s^2, that provides a comfortable braking experience
+    /// for passengers. This value is used to model typical braking behavior that minimizes discomfort
+    /// during deceleration.
+    /// </summary>
+    public readonly float ComfortableBraking = comfortableBraking;
+
+    /// <summary>
+    /// Represents the desired headway time, expressed in seconds, which a driver aims to maintain
+    /// from the vehicle ahead. This value influences the safe following distance and is a critical
+    /// parameter for traffic and vehicle behavior modeling.
+    /// </summary>
+    public readonly float HeadwayTime = headwayTime;
+
+    /// <summary>
+    /// Represents the minimum desired distance, measured in meters, that a vehicle should maintain
+    /// from the vehicle ahead during normal operation to ensure safety.
+    /// </summary>
+    public readonly float MinGap = minGap;
+
+    /// <summary>
+    /// Represents the acceleration exponent used in the Intelligent Driver Model (IDM).
+    /// This parameter influences the sensitivity of a vehicle's acceleration and deceleration
+    /// behavior, with higher values leading to more aggressive responses to speed adjustments.
+    /// </summary>
+    public readonly float AccelerationExponent = accelerationExponent;
+}
---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.Sim/Components/Vehicle/VehicleState.cs
@@ -0,0 +1,34 @@
+﻿namespace TrafficSimulation.Sim.Components;
+
+public struct VehicleState(int vehicleId, int laneIndex, float position, float speed, float acceleration, float length) {
+    /// <summary>
+    /// Represents the unique managed identifier for a vehicle.
+    /// </summary>
+    public int VehicleId = vehicleId;
+
+    /// <summary>
+    /// Represents the index of the lane in which the vehicle is currently positioned.
+    /// </summary>
+    public int LaneIndex = laneIndex;
+
+    /// <summary>
+    /// Represents the longitudinal position of the vehicle along its lane measured in meters.
+    /// This value indicates the vehicle's distance from the start of the lane.
+    /// </summary>
+    public float Position = position;
+
+    /// <summary>
+    /// Represents the current speed of a vehicle in m/s
+    /// </summary>
+    public float Speed = speed;
+
+    /// <summary>
+    /// Represents the acceleration for a vehicle in m/s².
+    /// </summary>
+    public float Acceleration = acceleration;
+
+    /// <summary>
+    /// Represents the length of the vehicle in meters.
+    /// </summary>
+    public float Length = length;
+}
