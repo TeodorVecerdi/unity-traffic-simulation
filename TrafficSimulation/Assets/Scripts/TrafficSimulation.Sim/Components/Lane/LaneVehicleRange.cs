5785cd9c2cc9b7ccbe3460466d7a81093ff29e0a
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-09-20 17:08:44 +0300

    Add components for lanes

---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.Sim/Components/Lane/LaneVehicleRange.cs
@@ -0,0 +1,19 @@
+﻿namespace TrafficSimulation.Sim.Components;
+
+/// <summary>
+/// Represents the array segment of vehicles within a specific lane in the global vehicle array.
+/// It allows efficient access to vehicles associated with a particular lane.
+/// </summary>
+/// <param name="start">The starting index of the vehicle range within the lane.</param>
+/// <param name="count">The total number of vehicles within the specified range in the lane.</param>
+public readonly struct LaneVehicleRange(int start, int count) {
+    /// <summary>
+    /// Represents the starting index of the range of vehicles within a lane.
+    /// </summary>
+    public readonly int Start = start;
+
+    /// <summary>
+    /// Represents the total number of vehicles within the specified range in a lane.
+    /// </summary>
+    public readonly int Count = count;
+}
