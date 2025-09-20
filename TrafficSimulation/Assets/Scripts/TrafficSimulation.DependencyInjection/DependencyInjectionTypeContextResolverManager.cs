8938dec02667e525c154789d0ad963998635428d
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-09-20 12:39:28 +0300

    Setup DI source generation

---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.DependencyInjection/DependencyInjectionTypeContextResolverManager.cs
@@ -0,0 +1,18 @@
+﻿using TrafficSimulation.Core;
+using TrafficSimulation.UI;
+using UnityEngine;
+using Vecerdi.Extensions.DependencyInjection;
+using Vecerdi.Extensions.DependencyInjection.Infrastructure;
+
+namespace TrafficSimulation.DependencyInjection;
+
+internal static class DependencyInjectionTypeContextResolverManager {
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    public static void InitializeTypeInjectorResolver() {
+        ServiceManager.Resolver = new TypeInjectorResolverCombiner(
+            new TrafficSimulationCoreTypeInjectorResolverContext(),
+            new TrafficSimulationUITypeInjectorResolverContext(),
+            new ReflectionTypeInjectorResolver()
+        );
+    }
+}
