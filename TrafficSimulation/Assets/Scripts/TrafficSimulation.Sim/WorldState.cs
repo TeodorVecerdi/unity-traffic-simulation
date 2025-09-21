9d5039b1fb139b58d4231a6874f57fc1785db32a
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-09-21 12:19:21 +0300

    Add WorldState class to store simulation data

---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.Sim/WorldState.cs
@@ -0,0 +1,43 @@
+﻿using TrafficSimulation.Sim.Components;
+using Unity.Collections;
+
+namespace TrafficSimulation.Sim;
+
+public sealed class WorldState(
+    NativeArray<VehicleState> vehicles,
+    NativeArray<IdmParameters> idmParameters,
+    NativeArray<float> accelerations,
+    NativeArray<LaneInfo> lanes,
+    NativeArray<LaneVehicleRange> laneRanges
+) : IDisposable {
+    // Per-vehicle data
+    public readonly NativeArray<VehicleState> Vehicles = vehicles;
+    public readonly NativeArray<IdmParameters> IdmParameters = idmParameters;
+    public readonly NativeArray<float> Accelerations = accelerations;
+
+    // Per-lane data
+    public readonly NativeArray<LaneInfo> Lanes = lanes;
+    public readonly NativeArray<LaneVehicleRange> LaneRanges = laneRanges;
+
+    public void Dispose() {
+        var vehicles = Vehicles;
+        if (vehicles.IsCreated)
+            vehicles.Dispose();
+
+        var idmParameters = IdmParameters;
+        if (idmParameters.IsCreated)
+            idmParameters.Dispose();
+
+        var accelerations = Accelerations;
+        if (accelerations.IsCreated)
+            accelerations.Dispose();
+
+        var lanes = Lanes;
+        if (lanes.IsCreated)
+            lanes.Dispose();
+
+        var laneRanges = LaneRanges;
+        if (laneRanges.IsCreated)
+            laneRanges.Dispose();
+    }
+}
