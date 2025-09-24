9db69ae8a908cc1ff6f9f5681e52a65c68f2deb0
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-09-24 20:46:31 +0300

    Add support for hard edges in polyline

--- TrafficSimulation/Assets/Scripts/TrafficSimulation.Geometry/Data/Polyline.cs
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.Geometry/Data/Polyline.cs
@@ -8,30 +8,43 @@ using UnityEditor;
 namespace TrafficSimulation.Geometry.Data;
 
 public sealed class Polyline : MonoBehaviour {
-    [SerializeField, Required] private List<float3> m_Points = [];
+    [SerializeField, Required] private List<PolylinePoint> m_Points = [];
     [SerializeField] private Color m_GizmoColor = Color.white;
+    [SerializeField] private Color m_HardGizmoColor = Color.cyan;
     [SerializeField, Min(0f)] private float m_SphereRadius = 0.075f;
 
-    public List<float3> Points => m_Points;
+    public List<float3> Points => ComputePoints();
+
+    private List<float3> ComputePoints() {
+        var pts = new List<float3>(m_Points.Count);
+        foreach (var p in m_Points) {
+            pts.Add(p.Position);
+            if (p.HardEdge) {
+                pts.Add(p.Position + new float3(1e-4f, 0f, 0f));
+            }
+        }
+        return pts;
+    }
 
     private void OnDrawGizmos() {
         if (m_Points == null! || m_Points.Count == 0)
             return;
 
-        Gizmos.color = m_GizmoColor;
 
         // Draw spheres at each point
         foreach (var p in m_Points) {
-            var worldPos = transform.TransformPoint(new Vector3(p.x, p.y, p.z));
+            var worldPos = transform.TransformPoint(p.Position);
+            Gizmos.color = p.HardEdge ? m_HardGizmoColor : m_GizmoColor;
             Gizmos.DrawSphere(worldPos, m_SphereRadius);
         }
 
         // Draw lines between consecutive points
+        Gizmos.color = m_GizmoColor;
         for (var i = 0; i < m_Points.Count - 1; i++) {
             var p1 = m_Points[i];
             var p2 = m_Points[i + 1];
-            var worldPos1 = transform.TransformPoint(new Vector3(p1.x, p1.y, p1.z));
-            var worldPos2 = transform.TransformPoint(new Vector3(p2.x, p2.y, p2.z));
+            var worldPos1 = transform.TransformPoint(p1.Position);
+            var worldPos2 = transform.TransformPoint(p2.Position);
             Gizmos.DrawLine(worldPos1, worldPos2);
         }
     }
@@ -47,9 +60,17 @@ public sealed class Polyline : MonoBehaviour {
 
         for (var i = 0; i < m_Points.Count; i++) {
             var p = m_Points[i];
-            var worldPos = transform.TransformPoint(new Vector3(p.x, p.y, p.z));
+            var worldPos = transform.TransformPoint(p.Position);
             Handles.Label(worldPos + Vector3.up * (m_SphereRadius * 1.5f), i.ToString(), style);
         }
 #endif
+    }
+
+    [Serializable]
+    private struct PolylinePoint {
+        [HideLabel]
+        public float3 Position;
+        [LabelText("Hard?")]
+        public bool HardEdge;
     }
 }
