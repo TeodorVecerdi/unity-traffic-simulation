e7f91ed4e34ff9f02d61830efb83068a78fd809c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-09-27 14:10:26 +0300

    Add new mesh pipeline implementation

---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.Geometry/Build/MeshGenerator.cs
@@ -0,0 +1,14 @@
+﻿using TrafficSimulation.Geometry.Data;
+using Unity.Jobs;
+
+namespace TrafficSimulation.Geometry.Build;
+
+public abstract class MeshGenerator {
+    public virtual void EstimateCounts(in MeshGenerationContext context, out int vertexCount, out int indexCount) {
+        vertexCount = 0;
+        indexCount = 0;
+    }
+
+    public abstract bool Validate();
+    public abstract JobHandle ScheduleGenerate(in MeshGenerationContext context, GeometryWriter writer, JobHandle dependency);
+}
---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.Geometry/Build/SurfaceAggregator.cs
@@ -0,0 +1,24 @@
+﻿using TrafficSimulation.Geometry.Data;
+using Unity.Collections;
+using UnityEngine;
+
+namespace TrafficSimulation.Geometry.Build;
+
+internal struct SurfaceAggregator(Material material, Allocator allocator) : IDisposable {
+    public readonly Material Material = material;
+    public NativeList<MeshVertex> Vertices = new(GeometryChunk.DefaultVertexCapacity, allocator);
+    public NativeList<int> Indices = new(GeometryChunk.DefaultIndexCapacity, allocator);
+
+    public void Dispose() {
+        if (Vertices.IsCreated) Vertices.Dispose();
+        if (Indices.IsCreated) Indices.Dispose();
+    }
+
+    public void AppendChunk(ref NativeList<MeshVertex> v, ref NativeList<int> i) {
+        var baseV = Vertices.Length;
+        Vertices.AddRange(v.AsArray());
+        for (var idx = 0; idx < i.Length; idx++) {
+            Indices.Add(i[idx] + baseV);
+        }
+    }
+}
