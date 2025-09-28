55fd6fe6d85b7496e59150b07acbd255c27340f8
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-09-28 14:03:36 +0300

    Add RoadSegmentAuthoring script

---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.RoadGraph/Authoring/RoadSegmentAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Sirenix.OdinInspector;
+using TrafficSimulation.RoadGraph.Configuration;
+using TrafficSimulation.RoadGraph.Data;
+using UnityEngine;
+
+namespace TrafficSimulation.RoadGraph.Authoring;
+
+public sealed class RoadSegmentAuthoring : MonoBehaviour {
+    [Title("Segment Configuration")]
+    [SerializeField, InlineProperty, HideLabel] private RoadSegment m_SegmentConfiguration;
+    [SerializeField, Required] private RoadMarkingConfiguration m_RoadMarkingConfiguration = null!;
+    [SerializeField, Required] private SidewalkConfiguration m_SidewalkConfiguration = null!;
+
+    public RoadSegment RoadSegment => m_SegmentConfiguration;
+    public RoadMarkingConfiguration RoadMarkingConfiguration => m_RoadMarkingConfiguration;
+    public SidewalkConfiguration SidewalkConfiguration => m_SidewalkConfiguration;
+}
