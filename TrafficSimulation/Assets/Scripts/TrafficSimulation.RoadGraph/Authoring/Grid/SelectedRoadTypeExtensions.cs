c3e0c859623f887360c72ba3670e88b627022a3b
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-10-04 09:35:43 +0300

    Add GetGridOffset extension for SelectedRoadType

--- TrafficSimulation/Assets/Scripts/TrafficSimulation.RoadGraph/Authoring/Grid/SelectedRoadTypeExtensions.cs
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.RoadGraph/Authoring/Grid/SelectedRoadTypeExtensions.cs
@@ -5,12 +5,17 @@ namespace TrafficSimulation.RoadGraph.Authoring.Grid;
 public static class SelectedRoadTypeExtensions {
     extension(SelectedRoadType type) {
         public int2 GetRoadSpan() => type switch {
-            SelectedRoadType.None => new int2(1, 1),
+            SelectedRoadType.None => new int2(0, 0),
             SelectedRoadType.OneLane => new int2(1, 1),
             SelectedRoadType.TwoLane => new int2(2, 1),
             SelectedRoadType.ThreeLane => new int2(3, 1),
             SelectedRoadType.FourLane => new int2(4, 1),
-            _ => new int2(1, 1),
+            _ => new int2(0, 0),
         };
+
+        public float2 GetGridOffset() {
+            var span = type.GetRoadSpan();
+            return 0.5f * new float2(span);
+        }
     }
 }
