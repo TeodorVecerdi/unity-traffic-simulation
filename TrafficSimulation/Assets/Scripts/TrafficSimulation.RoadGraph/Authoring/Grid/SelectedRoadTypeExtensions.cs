d26dca52ecf77abafab1ef96948f237fcb4bfd9c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-10-04 09:16:39 +0300

    Ad GetRoadSpan extension for SelectedRoadType

---
+++ TrafficSimulation/Assets/Scripts/TrafficSimulation.RoadGraph/Authoring/Grid/SelectedRoadTypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+
+namespace TrafficSimulation.RoadGraph.Authoring.Grid;
+
+public static class SelectedRoadTypeExtensions {
+    extension(SelectedRoadType type) {
+        public int2 GetRoadSpan() => type switch {
+            SelectedRoadType.None => new int2(1, 1),
+            SelectedRoadType.OneLane => new int2(1, 1),
+            SelectedRoadType.TwoLane => new int2(2, 1),
+            SelectedRoadType.ThreeLane => new int2(3, 1),
+            SelectedRoadType.FourLane => new int2(4, 1),
+            _ => new int2(1, 1),
+        };
+    }
+}
